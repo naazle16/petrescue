853f71e76d07fbadfe56e512e4e5a0b7c2d2e635
Humazah <your_email@example.com>
2025-10-27 21:07:00 +0530

    Initial commit - PetRescue Milestone 1

---
+++ petusers/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
+
+def home(request):
+    return render(request, 'home.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth_login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid username or password'})
+
+    return render(request, 'login.html')
+
+
+
+
+
